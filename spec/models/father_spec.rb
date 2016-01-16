6480152c959d53b2fd5af83007de491a304ca249
Tom Futureworkz <tom@futurewokz.com>
2016-01-16 14:20:02 +0700

    Add Father#say_something test case

--- spec/models/father_spec.rb
+++ spec/models/father_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe Father, type: :model do
   context 'validations' do
     it { is_expected.to enumerize(:gender).in(:male) }
   end  
+
+  describe '#say_something' do
+    let(:father)   { create(:father) }
+    let(:tom)      { create(:son, father: father) }
+    let(:greeting) { "Hello, I am your father" }
+
+    it 'returns the greeting of father' do
+      expect(tom.father.say_something).to eq greeting
+    end
+  end
 end
